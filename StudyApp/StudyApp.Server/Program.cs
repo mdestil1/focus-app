572e89218099e1eecb746904fae2daae27d45b52
sydney <sydm@bu.edu>
2024-12-09 03:36:37 -0500

    StudyApp with the front end form in pixelaSignUp passing information to the backend PixelaController.

--- StudyApp/StudyApp.Server/Program.cs
+++ StudyApp/StudyApp.Server/Program.cs
@@ -1,13 +1,31 @@
+using StudyApp.Server;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost", builder =>
+    {
+        builder.WithOrigins("https://localhost:5173") // Allow React app origin
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddHttpClient<PixelaSignUpService>(client =>
+{
+    // Optionally configure the HttpClient here
+    client.BaseAddress = new Uri("https://pixe.la"); // Example base URL
+    client.DefaultRequestHeaders.Add("User-Agent", "PixelaClientApp");
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
@@ -19,12 +37,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Apply the CORS policy
+
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseRouting();
+app.UseCors("AllowLocalhost");
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
+
 
 app.Run();
