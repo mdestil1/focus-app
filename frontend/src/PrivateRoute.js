e32fde4bba178f288af2efe71b223a42fd207d5b
mdestil1 <mdestil@bu.edu>
2024-12-09 23:47:04 -0500

    db

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -1,19 +1,11 @@
+// PrivateRoute.js
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
-  const isPaid = localStorage.getItem('isPaid') === 'true';
 
-  if (!token) {
-    return <Navigate to="/login" replace />;
-  }
-
-  if (!isPaid) {
-    return <Navigate to="/checkout" replace />;
-  }
-
-  return children;
+  return token ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
